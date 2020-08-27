e7f9b6e1ac3e0093da0597ceaa122d3609d3535c
vaishak nambiar <vgnambiar7@gmail.com>
2020-08-27 22:02:58 +0530

    removed duplicate directory

--- emp_mgt_system/delete_record.py
+++
@@ -1,7 +0,0 @@
-def delete(eid, lst):
-    for i in range(len(lst)):
-        if lst[i]["emp_id"] == eid:
-           # print(lst[i])
-            del lst[i]
-
-    return lst
--- emp_mgt_system/insert_record.py
+++
@@ -1,3 +0,0 @@
-def Insert(n, i, e, d):
-    dic = {'emp_name': n, 'emp_id': i, 'emp_email': e, 'emp_dob': d}
-    return dic
